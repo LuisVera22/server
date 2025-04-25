4b2d6092e13d1dddbbd201ed20da99f71f6c4175
Luis David Vera Vilchez <luis22vera03@gmail.com>
2025-04-24 23:16:27 -0500

    🎉 Subiendo el proyecto base.

---
+++ src/main/java/com/salonperu/server/SalonperuApplication.java
@@ -0,0 +1,13 @@
+package com.salonperu.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonperuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonperuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salonperu/server/SalonperuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salonperu.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonperuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
